2edbb8ba8350296f5c0a463498c7d2a0b6b85f2b
Matija Lukic <matija.lukic003@gmail.com>
2026-05-27 17:13:43 +0200

    Initial commit

---
+++ src/main/java/rs/fon/bg/ac/rs/lukic_matija/LukicMatijaApplication.java
@@ -0,0 +1,13 @@
+package rs.fon.bg.ac.rs.lukic_matija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LukicMatijaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LukicMatijaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/fon/bg/ac/rs/lukic_matija/LukicMatijaApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.fon.bg.ac.rs.lukic_matija;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LukicMatijaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
