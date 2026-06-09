a76b3ac807551269c550b20c70310f8217a56ecd
Matija Lukic <matija.lukic003@gmail.com>
2026-06-09 11:32:29 +0200

    Add match validation logic, fix

--- src/main/java/rs/fon/bg/ac/rs/lukic_matija/dtos/matchDtos/MatchUpdateDto.java
+++ src/main/java/rs/fon/bg/ac/rs/lukic_matija/dtos/matchDtos/MatchUpdateDto.java
@@ -1,4 +1,10 @@
 package rs.fon.bg.ac.rs.lukic_matija.dtos.matchDtos;
 
-public record MatchUpdateDto(int homePoints,int awayPoints) {
+import jakarta.validation.constraints.Min;
+
+public record MatchUpdateDto(
+        @Min(value = 0, message = "Home team points cant be negative")
+        int homePoints,
+        @Min(value = 0, message = "Away team points cant be negative")
+        int awayPoints) {
 }
